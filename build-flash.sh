cba358ad415b946dc9bc386fbae57dba7551778a
Yong Li <yong.li@intel.com>
2024-06-19 14:01:46 +0800

    add build script

---
+++ build-flash.sh
@@ -0,0 +1,22 @@
+pushd test_context_switch
+make
+popd
+
+pushd pi-opensbi
+CROSS_COMPILE=/opt/riscv/bin/riscv64-unknown-linux-gnu- make -j$(nproc) PLATFORM_DEFCONFIG=k1_defconfig PLATFORM=generic BUILD_INFO=y
+export OPENSBI=`realpath build/platform/generic/firmware/fw_dynamic.bin`
+popd
+
+pushd pi-u-boot
+CROSS_COMPILE=/opt/riscv/bin/riscv64-unknown-linux-gnu- make -j8
+cp ../pi-opensbi/build/platform/generic/firmware/fw_dynamic.itb .
+cp ../test_context_switch/build/s-hello/s-hello.bin .
+cp ../test_context_switch/build/ns-hello/ns-hello.bin .
+truncate -s %64 s-hello.bin
+truncate -s %64 ns-hello.bin
+./tools/mkimage -f u-boot-new.its -A riscv -O u-boot -T firmware u-boot-new.itb
+sudo dd if=u-boot-new.itb of=/dev/sde4
+sudo dd if=fw_dynamic.itb of=/dev/sde3
+sudo dd if=FSBL.bin of=/dev/sde1
+sync
+popd
