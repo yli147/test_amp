296731f17fc8ac2ca9808d37884826c079fba712
Yong Li <yong.li@intel.com>
2024-06-19 17:00:44 +0800

    Update build script

--- build-flash.sh
+++ build-flash.sh
@@ -2,14 +2,14 @@ pushd test_context_switch
 make
 popd
 
-pushd pi-opensbi
+pushd opensbi
 CROSS_COMPILE=/opt/riscv/bin/riscv64-unknown-linux-gnu- make -j$(nproc) PLATFORM_DEFCONFIG=k1_defconfig PLATFORM=generic BUILD_INFO=y
 export OPENSBI=`realpath build/platform/generic/firmware/fw_dynamic.bin`
 popd
 
-pushd pi-u-boot
+pushd u-boot
 CROSS_COMPILE=/opt/riscv/bin/riscv64-unknown-linux-gnu- make -j8
-cp ../pi-opensbi/build/platform/generic/firmware/fw_dynamic.itb .
+cp ../opensbi/build/platform/generic/firmware/fw_dynamic.itb .
 cp ../test_context_switch/build/s-hello/s-hello.bin .
 cp ../test_context_switch/build/ns-hello/ns-hello.bin .
 truncate -s %64 s-hello.bin
